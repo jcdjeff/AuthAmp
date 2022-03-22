4a5a130e48b1cf5d10611bc31a21453e4ffb98db
webappjeff <design@webappjeff.com>
2022-03-22 13:35:34 -0400

    First Login

--- App.tsx
+++ App.tsx
@@ -4,8 +4,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import Amplify, {Auth} from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+import {withAuthenticator} from 'aws-amplify-react-native';
 
-export default function App() {
+Amplify.configure(awsconfig)
+
+const App = () => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -20,3 +25,40 @@ export default function App() {
     );
   }
 }
+
+const signUpConfig = {
+  header: "My Customized Sign Up on Top",
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: "Full name",
+      key: "name",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 2,
+      type: "string",
+    },
+    {
+      label: "Username",
+      key: "preferred_username",
+      required: true,
+      displayOrder: 3,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 4,
+      type: "password",
+    },
+  ],
+};
+
+export default withAuthenticator(App, { signUpConfig });
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,39 @@
-import { StyleSheet } from 'react-native';
+import { Alert, Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import Amplify, {Auth} from 'aws-amplify'
+import React from 'react';
+import Colors from '../constants/Colors';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+const TabOneScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
+
+  async function onLogout () {
+        Auth.signOut()
+        }
+        
+  const twoButtonAlert = () =>
+        Alert.alert('Logout', 'Are you sure you want to log out?', [
+        {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+        },
+        { text: 'Log out', onPress: () => onLogout() },
+        ]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Button
+        title="Logout"
+        color={Colors.light.tint}
+        // onPress={onLogout}
+        onPress={twoButtonAlert}
+    />
     </View>
   );
 }
@@ -30,3 +54,5 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+export default TabOneScreen;
